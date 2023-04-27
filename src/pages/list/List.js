4d4e24e84fc5e6ae00b82243594d1f3163acc448
rajendra kumar <rajendrakumar25.rk11@gmail.com>
2023-04-27 11:05:29 +0530

    first commit

---
+++ src/pages/list/List.js
@@ -0,0 +1,20 @@
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
+import Datatable from "../.././components/datatable/DataTable"
+
+import "./list.scss"
+
+const List = () => {
+  return (
+    <div className="list">
+      <Sidebar/>
+      <div className="listContainer">
+        <Navbar/>
+        <Datatable/>
+      </div>
+    </div>
+  )
+}
+
+export default List
+ 
